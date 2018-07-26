332ed5be93a8647e739aea425f9c2ddd0b1271f1
sashadev-sky <sboginsky17@gmail.com>
2018-07-25 23:07:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Minesweeper
+
+#### Ruby Concepts
+
+- Save/load functionality using YAML
+- ARGV to load in file
+- File class I/O
+- Freezing constants
+- Recursion to simplify complicated logic
+- Colorize Gem
+- Overriding Inspect
---
+++ tile.rb
@@ -0,0 +1,110 @@
+require 'colorize'
+
+class Tile
+  DELTAS = [
+    [-1, -1],
+    [-1,  0],
+    [-1,  1],
+    [ 0, -1],
+    [ 0,  1],
+    [ 1, -1],
+    [ 1,  0],
+    [ 1,  1]
+  ].freeze
+
+  attr_reader :pos
+
+  def initialize(board, pos)
+    @board, @pos = board, pos
+    @bombed, @explored, @flagged = false, false, false
+  end
+
+  def bombed?
+    @bombed
+  end
+
+  def explored?
+    @explored
+  end
+
+  def flagged?
+    @flagged
+  end
+
+  def adjacent_bomb_count
+    neighbors.select(&:bombed?).count
+  end
+
+  def explore
+    # don't explore a location user thinks is bombed.
+    return self if flagged?
+
+    # don't revisit previously explored tiles
+    return self if explored?
+
+    @explored = true
+    if !bombed? && adjacent_bomb_count == 0
+      neighbors.each(&:explore)
+    end
+
+    self
+  end
+
+  def inspect
+    { pos: pos,
+      bombed: bombed?,
+      flagged: flagged?,
+      explored: explored? }.inspect
+  end
+
+  def neighbors
+    adjacent_coords = DELTAS.map do |(dx, dy)|
+      [pos[0] + dx, pos[1] + dy]
+    end.select do |row, col|
+      [row, col].all? do |coord|
+        coord.between?(0, @board.grid_size - 1)
+      end
+    end
+
+    adjacent_coords.map { |pos| @board[pos] }
+  end
+
+  def plant_bomb
+    @bombed = true
+  end
+
+  def color
+    case adjacent_bomb_count
+    when 1 then :blue
+    when 2 then :red
+    when 3 then :green
+    end
+  end
+
+  def render
+    if flagged?
+      "F"
+    elsif explored?
+      adjacent_bomb_count == 0 ? "_" : adjacent_bomb_count.to_s.colorize(color)
+    else
+      "*"
+    end
+  end
+
+  def reveal
+    # used to fully reveal the board at game end
+    if flagged?
+      # mark true and false flags
+      bombed? ? "F" : "f"
+    elsif bombed?
+      # display a hit bomb as an X
+      explored? ? "X".colorize(:red) : "B".colorize(:red)
+    else
+      adjacent_bomb_count == 0 ? "_" : adjacent_bomb_count.to_s
+    end
+  end
+
+  def toggle_flag
+    @flagged = !@flagged unless @explored
+  end
+end
